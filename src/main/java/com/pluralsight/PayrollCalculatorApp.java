8d2c9bb9352dfbb6be9c4362a1505d267a63b4f4
chrisbunny <christopherjbunny@gmail.com>
2026-04-21 13:59:57 -0500

    Finished

--- src/main/java/com/pluralsight/PayrollCalculatorApp.java
+++ src/main/java/com/pluralsight/PayrollCalculatorApp.java
@@ -1,14 +1,29 @@
 package com.pluralsight;
+import java.util.*;
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.FileReader;
-import java.util.Arrays;
+import java.io.FileWriter;
 
 public class PayrollCalculatorApp {
+    static Scanner myScanner = new Scanner(System.in);
     public static void main(String[] args) {
 
+        //prompting the user for file name to input into the filWriter
+        System.out.println("Enter the name of the file employee file to process: ");
+        String userFile = myScanner.nextLine();
+        System.out.println("Enter the name of the payroll file to create: ");
+        String userPrFile = myScanner.nextLine();
+
         try {
-            FileReader fileReader = new FileReader("src/main/resources/employees.csv");
+            //file reader for userFile
+            FileReader fileReader = new FileReader("src/main/resources/" + userFile);
             BufferedReader theFile = new BufferedReader(fileReader);
+
+            //file writer for userFile
+            FileWriter fileWriter = new FileWriter("src/main/resources/" + userPrFile, true);
+            BufferedWriter bufWriter = new BufferedWriter(fileWriter);
+
 
             String line;
             //initialized line number at 0
@@ -21,7 +36,7 @@ public class PayrollCalculatorApp {
                 if (lineNum == 1) {
                     continue;
                 }
-                theFile.readLine();
+                //theFile.readLine();
 
                 //process the other rows
                 //split the row on the |
@@ -37,7 +52,6 @@ public class PayrollCalculatorApp {
                 //create an employee from the employee class and use the setters
                 //to set the properties from the row in the file
 
-
                 Employee employee = new Employee(id, name, hoursWorked, payRate);
 
                 //prints out to see if I store the employee properties properly
@@ -51,9 +65,14 @@ public class PayrollCalculatorApp {
                 //System.out.println(line);
 
                 System.out.printf("ID: %d | %s's gross pay: %.2f\n", employee.getEmployeeID(), employee.getName(), employee.getGrossPay());
+
+                bufWriter.write(employee.getEmployeeID() + " | " + employee.getName() + " | " + employee.getGrossPay() + "\n");
             }
+            fileWriter.close();
+            bufWriter.close();
+
         } catch (Exception e) {
-            throw new RuntimeException(e);
+            System.out.println("File not found.");
         }
 
     }
